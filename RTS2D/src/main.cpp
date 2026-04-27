0d582c8af926569a8cc3a7e49e9fb7a507ad22ad
Elhyareno <mathieu.groult@gmail.com>
2026-04-27 20:25:53 +0200

    fix

---
+++ RTS2D/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Bienvenue dans RTS2D !" << std::endl;
+
+    int nourriture = 50;
+    int bois = 120;
+    int or = 30;
+
+    std::cout << "Nourriture : " << nourriture << std::endl;
+    std::cout << "Bois : " << bois << std::endl;
+    std::cout << "Or : " << or << std::endl;
+
+    return 0;
+}
